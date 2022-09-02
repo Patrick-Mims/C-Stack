84be726568670d75be46b13720b7d15b870ab211
Patrick <patrickkevinmims@gmail.com>
2022-09-01 21:02:35 -0700

    Implemented io.h, ioImpl.c

--- src/client.c
+++ src/client.c
@@ -26,6 +26,9 @@ int main(void)
 
   push(stack, 9000);
 
+  read_data();
+
+
   /*comment for testing*/
 
   return 0;
--- src/ioImpl.c
+++ src/ioImpl.c
@@ -1,8 +1,29 @@
-
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+#include "io.h"
+#include "stack.h"
+
+#define INDEX 100
+
+void read_data()
 {
-  return 0;
+  stack_t stack = new_stack(5);
+
+  const int size = 100;
+  FILE *fp = NULL;
+  char line[INDEX] = {0};
+
+  if((fp = fopen("names.txt", "r")) == NULL) 
+  {
+    printf("File doesn't exist\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while(fgets(line, size, fp))
+  {
+    printf("%s\n", line);
+  }
+
+  fclose(fp);
 }
